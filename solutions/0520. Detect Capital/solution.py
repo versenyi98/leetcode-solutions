c0e47c20c4c9f49f50a55917633a18ded623a124
Péter Versényi <versenyi98@gmail.com>
2023-01-02 18:11:29 +0100

    Use builtins

--- solutions/0520. Detect Capital/solution.py
+++ solutions/0520. Detect Capital/solution.py
@@ -1,3 +1,3 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        return all(ch.islower() for ch in word[1:]) or all(ch.isupper() for ch in word)
+        return word.isupper() or word.islower() or word.istitle()
