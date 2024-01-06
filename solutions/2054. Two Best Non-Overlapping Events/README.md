ece0394d586dff3fa95398cb2c1af660a5cc86f0
Péter Versényi <versenyi98@gmail.com>
2024-01-06 15:03:57 +0100

    Add "2054. Two Best Non-Overlapping Events"

---
+++ solutions/2054. Two Best Non-Overlapping Events/README.md
@@ -0,0 +1,3 @@
+| ID | Link to description | Link to solution
+|:---|:---|:---:|
+| 2054 | [Two Best Non-Overlapping Events](https://leetcode.com/problems/two-best-non-overlapping-events/) | [Solution](https://github.com/versenyi98/leetcode-solutions/tree/main/solutions/2054.%20Two%20Best%20Non-Overlapping%20Events)|
---
+++ solutions/2054. Two Best Non-Overlapping Events/main.py
@@ -0,0 +1,31 @@
+class Solution:
+    def dp(self, i, k):
+        if k == 0:
+            return 0
+
+        if i == len(self.s):
+            return 0
+
+        if self.cache[k][i] != -1:
+            return self.cache[k][i]
+
+        ni = bisect_right(self.s, self.e[i])
+        self.cache[k][i] = max(self.dp(ni, k - 1) + self.p[i], self.dp(i + 1, k))
+        return self.cache[k][i]
+
+    def maxTwoEvents(self, events: List[List[int]]) -> int:
+        self.s = []
+        self.e = []
+        self.p = []
+
+        self.cache = []
+        k = 2
+        for i in range(k + 1):
+            self.cache.append([-1] * len(events))
+
+        for s, e, p in sorted(events):
+            self.s.append(s)
+            self.e.append(e)
+            self.p.append(p)
+
+        return self.dp(0, k)
